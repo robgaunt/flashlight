6ad3c2816047a487dbaea3fa68ecc6a0abc6dfa0
Rob Gaunt <robgaunt@google.com>
2013-08-10 15:09:35 -0700

    adding files back...

---
+++ python/searchlight.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python
+
+"""Executable script which runs an OSC server to control a single searchlight.
+
+Example usage:
+  ./searchlight.py --osc_name 1 --osc_address 224.0.0.1 --osc_listen_port 8888 \
+                   --serial_address /dev/ttyUSB1
+"""
+
+__author__ = 'Rob Gaunt (robgaunt@gmail.com)'
+
+import argparse
+import logging
+from twisted.internet import reactor
+
+import motor_controller
+import searchlight_application
+
+
+def configure_logging(logfile):
+  logging.basicConfig(level=logging.DEBUG,
+                      format='%(asctime)s %(levelname)-8s %(message)s',
+                      filename=logfile,
+                      filemode='w')
+
+
+def main():
+  parser = argparse.ArgumentParser(description='OSC server to control a single searchlight.')
+  parser.add_argument('--osc_address', type=str, required=True,
+                      help='Multicast address to listen for OSC messages.')
+  parser.add_argument('--osc_listen_port', type=int, required=True,
+                      help='Port to listen for OSC messages.')
+  parser.add_argument('--osc_name', type=str, required=True,
+                      help='Prefix name for all OSC endpoints.')
+  parser.add_argument('--serial_address', type=str, required=True,
+                      help='Address of serial device to communicate with the controller.')
+  parser.add_argument('--controller_channels', type=int, default=1,
+                      help='Number of motor control channels supported by the controller.')
+  parser.add_argument('--logfile', type=str, default='/tmp/searchlight.log')
+  args = parser.parse_args()
+
+  configure_logging(args.logfile)
+
+  controller = motor_controller.MotorController(
+      reactor, args.serial_address, args.controller_channels)
+  searchlight_application.SearchlightApplication(
+      reactor, controller, args.osc_name, args.osc_address, args.osc_listen_port)
+  reactor.run()
+
+
+if __name__ == '__main__':
+  main()
---
+++ python/searchlight_application.py
@@ -0,0 +1,53 @@
+__author__ = 'Rob Gaunt (robgaunt@gmail.com)'
+
+import logging
+from txosc import async
+from txosc import dispatch
+
+SEARCHLIGHT_NAME_ALL = "all"
+
+
+class SearchlightApplication(object):
+  """Serves as the wiring between the OSC server and motor controller.
+
+  This registers OSC addresses with a txosc Receiver, and translates OSC commands to motor
+  controller commands.
+
+  All OSC addresses are of the form /<searchlight name>/<command>. Each searchlight registers for
+  addresses using both its name and the name "all". This makes it possible to send OSC commands
+  either to individual searchlights, or to all searchlights at the same time.
+  """
+
+  def __init__(self, reactor, motorController, name, address, listenPort):
+    assert name != SEARCHLIGHT_NAME_ALL, 'Name %s is reserved' % SEARCHLIGHT_NAME_ALL
+    self.name = name
+    self.motorController = motorController
+    self.oscReceiver = dispatch.Receiver()
+    self.addOscCallback("go1", self.motor_1_go)
+    self.addOscCallback("go2", self.motor_2_go)
+    reactor.listenMulticast(
+        listenPort,
+        async.MulticastDatagramServerProtocol(self.oscReceiver, address),
+        listenMultiple=True)
+
+  def addOscCallback(self, callbackName, callback):
+    self.oscReceiver.addCallback("/%s/%s" % (self.name, callbackName), callback)
+    self.oscReceiver.addCallback("/%s/%s" % (SEARCHLIGHT_NAME_ALL, callbackName), callback)
+
+  def unwrap_osc(func):
+    """Decorator for all OSC handlers."""
+    def handler(self, message, address):
+      logging.debug('Searchlight %s received %s from %s', self.name, message, address)
+      func(self, *message.getValues())
+    return handler
+
+  @unwrap_osc
+  def motor_1_go(self, value):
+    self.motorController.go(1, value * 1000)
+
+  @unwrap_osc
+  def motor_2_go(self, value):
+    if 2 <= self.motorController.numChannels:
+      self.motorController.go(2, value * 1000)
+    else:
+      logging.info('Searchlight %s ignoring command to motor channel 2', self.name)
