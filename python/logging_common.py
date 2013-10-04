6d2011167d7e22c87dd7c89130bfa8d82d9bf15e
Rob Gaunt <robgaunt@google.com>
2013-10-03 21:20:38 -0700

    Adding missing logging module and touch OSC layout.

---
+++ python/logging_common.py
@@ -0,0 +1,24 @@
+"""Utilities for configuring logging parameters."""
+
+__author__ = 'Rob Gaunt (robgaunt@gmail.com)'
+
+import logging
+from twisted.python import log
+
+
+def configure_logging(logfile):
+  logging.basicConfig(level=logging.DEBUG,
+                      format='%(asctime)s %(levelname)-8s %(message)s',
+                      filename=logfile,
+                      filemode='w')
+  # Configure Twisted to log to our logfile too.
+  observer = log.PythonLoggingObserver()
+  observer.start()
+
+
+def add_logging_args(argument_parser):
+  argument_parser.add_argument('--logfile', type=str, default='/tmp/searchlight.log')
+
+
+def configure_logging_from_args(args):
+  configure_logging(args.logfile)
