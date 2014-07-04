75b590e9bd852cb55168e2c4fdb0adfcfb3f7d8b
Rob Gaunt <robgaunt@gmail.com>
2014-07-04 16:11:53 -0700

    Checking in files for PSMove connection support

---
+++ python/psmove_connection_manager.py
@@ -0,0 +1,98 @@
+import psmove
+import logging
+
+import psmove_controller
+
+PSMOVE_CONNECT_INTERVAL = 5
+
+
+class PSMoveConnectionManager(object):
+  """Manages connecting and disconnecting PSMove controllers."""
+
+  def __init__(self, reactor, controller_configs, name_to_searchlight):
+    self.reactor = reactor
+    self.name_to_searchlight_ = name_to_searchlight
+    # As we connect PSMove controllers, we pop an element off of unconnected_configs_ and move it
+    # over to serial_to_config_. As we disconnect them, we do the reverse. This assigns
+    # configurations to the controllers in the order that they are connected.
+    self.unconnected_configs_ = controller_configs
+    # Maps PSMove serial (as string) to configuration dictionary.
+    self.serial_to_config_ = {}
+    # Maps PSMove serial (as string) to PSMoveController instance.
+    self.serial_to_controller_ = {}
+    # Set of serials for which we already attempted USB pairing. This is kept so that we don't
+    # attempt to pair any controller more than once.
+    self.paired_usb_serials_ = set()
+    self.reactor.callLater(0, self.connect_moves_)
+
+  def connect_moves_(self):
+    self.reactor.callLater(PSMOVE_CONNECT_INTERVAL, self.connect_moves_)
+
+    connection_count = psmove.count_connected()
+    connected_serials = set()
+    logging.debug('Connected Moves: %d', connection_count)
+    for psmove_id in xrange(connection_count):
+      move = psmove.PSMove(psmove_id)
+      serial = move.get_serial()
+      logging.debug('Detected move %d serial %s connection %s',
+                    psmove_id, serial, self.debug_connection_type_(move))
+      connected_serials.add(serial)
+
+      if move.connection_type == psmove.Conn_USB and serial not in self.paired_usb_serials_:
+        self.pair_move_(serial, move)
+        del move
+      elif (move.connection_type == psmove.Conn_Bluetooth and
+          serial not in self.serial_to_controller_ and
+          self.unconnected_configs_):
+        self.connect_move_(serial, move)
+      else:
+        del move
+
+    self.remove_disconnected_controllers_(connected_serials)
+
+  def pair_move_(self, serial, move):
+    """Pairs a USB Move so that it may be connected via Bluetooth.
+
+    It should only be necessary to execute this once each time the application is started. After
+    that the Move should already be paired and connect automatically when you press the PS button.
+    """
+    logging.info('Pairing move: %s', serial)
+    move.pair()
+    # Turn LEDs green to signify pairing completion.
+    move.set_leds(0, 255, 0)
+    move.update_leds()
+    self.paired_usb_serials_.add(serial)
+
+  def connect_move_(self, serial, move):
+    """Connects a Bluetooth Move by creating a PSMoveController instance for it."""
+    move.enable_orientation(True)
+    has_calibration = move.has_calibration()
+    has_orientation = move.has_orientation()
+    if has_calibration and has_orientation:
+      config = self.unconnected_configs_.pop()
+      logging.info('Connecting move: %s config: %s', serial, config)
+      searchlights = [self.name_to_searchlight_[s] for s in config['searchlight_names']]
+      self.serial_to_config_[serial] = config
+      self.serial_to_controller_[serial] = psmove_controller.PSMoveController(
+          self.reactor, move, serial, searchlights)
+    else:
+      logging.error('Unable to connect move: %s - missing calibration (%d) or orientation (%d)',
+                    serial, has_calibration, has_orientation)
+      # Turn LEDs red to signify connection error.
+      move.set_leds(255, 0, 0)
+      move.update_leds()
+
+  def remove_disconnected_controllers_(self, connected_serials):
+    missing_serials = [s for s in self.serial_to_controller_ if s not in connected_serials]
+    for serial in missing_serials:
+      logging.info('PSMove with serial %s went missing.', serial)
+      self.paired_usb_serials_.discard(serial)
+      self.unconnected_configs_.append(self.serial_to_config_.pop(serial))
+      controller = self.serial_to_controller_.pop(serial)
+      controller.disconnect()
+
+  def debug_connection_type_(self, move):
+    if move.connection_type == psmove.Conn_USB:
+      return 'USB'
+    else:
+      return 'Bluetooth'
---
+++ python/psmove_controller.py
@@ -0,0 +1,97 @@
+import logging
+import math
+import psmove
+import time
+
+from twisted.internet import task
+
+PSMOVE_CONTROL_LOOP_INTERVAL = 0.2
+
+
+class PSMoveController(object):
+  """Acts as an interface between a PSMove controller and searchlights."""
+  def __init__(self, reactor, move, serial, searchlights):
+    """Creates a PSMove controller.
+
+    Args:
+      reactor: The twisted.internet.reactor module.
+      move: A PSMove instance.
+      serial: The serial number of the move, as a string.
+      searchlights: A list of searchlight.Searchlight instances that this controls.
+    """
+    self.reactor = reactor
+    self.move = move
+    self.serial = serial
+    self.searchlights = searchlights
+    self.set_orientation_enabled_(False)
+    self.reactor.addSystemEventTrigger('before', 'shutdown', self.before_shutdown_)
+    self.task_ = task.LoopingCall(self.control_loop_)
+    self.task_.start(PSMOVE_CONTROL_LOOP_INTERVAL)
+
+  def disconnect(self):
+    self.task_.stop()
+    del self.move
+    self.move = None
+
+  def control_loop_(self):
+    """Main control loop which gets orientation and sends commands."""
+    if self.move.poll():
+      pressed, released = self.move.get_button_events()
+      if pressed & psmove.Btn_START:
+        logging.info('Resetting PSMove %s orientation.', self.serial)
+        self.move.reset_orientation()
+        self.set_orientation_enabled_(True)
+
+      if pressed & psmove.Btn_SELECT:
+        self.set_orientation_enabled_(False)
+
+      if self.orientation_enabled:
+        self.update_searchlight_from_orientation_()
+
+    # This must be called periodically to keep the LEDs on, even if we do not change the values.
+    self.update_leds_()
+
+  def before_shutdown_(self):
+    if self.move:
+      self.disconnect()
+
+  def set_orientation_enabled_(self, orientation_enabled):
+    self.orientation_enabled = orientation_enabled
+    self.update_leds_()
+
+  def update_leds_(self):
+    # For some unknown reason, if you always tell the move to change to a single color, it doesn't
+    # work. (Even if you turn off rate limiting). It seems that you need to make gradual changes
+    # to the color over time to make it stick. That doesn't seem right, so I must be missing
+    # something.
+    scale = 0.8 + 0.2 * math.sin(int(10 * time.time()) / 10.0)
+    if self.orientation_enabled:
+      self.move.set_leds(0, int(scale * 255), 0)  # Green
+    else:
+      self.move.set_leds(int(scale * 255), int(scale * 255), 0)  # Yellow
+    self.move.update_leds()
+
+  def update_searchlight_from_orientation_(self):
+    qw, qx, qy, qz = self.move.get_orientation()
+    # Convert quaternion to euler-angle.
+    # Formulas from:
+    # http://www.euclideanspace.com/maths/geometry/rotations/conversions/quaternionToEuler/
+    # but note that all values are negated.
+    azimuth = 0
+    elevation = 0
+    test = qx * qy + qz * qw
+    roll = math.asin(2 * test)
+    if test > 0.499:
+      azimuth = -2 * math.atan2(qx, qw)
+      elevation = math.pi / 2
+    elif test < -0.499:
+      azimuth = 2 * math.atan2(qx, qw)
+      elevation = 0
+    else:
+      azimuth = -math.atan2(2 * qy * qw - 2 * qx * qz,
+                            1 - 2 * qy * qy - 2 * qz * qz)
+      elevation = math.atan2(2 * qx * qw - 2 * qy * qz ,
+                             1 - 2 * qx * qx - 2 * qz * qz)
+    for searchlight in self.searchlights:
+      searchlight.target_angle(azimuth, elevation)
+
